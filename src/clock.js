be54a6b6129ea13f65ccb7692b86dc33009781e1
marcelojcs <marcelojcs@outlook.com>
2019-04-29 23:57:02 -0300

    clock func px test

--- src/clock.js
+++ src/clock.js
@@ -1,8 +1,21 @@
 import Reacet from 'react';
+var s;
+var m;
+var h;
+function manageClock(){
+  if (s>60){
+    m+=1
+    s=0;
+  }
+  if (m>60){
+    h+=1
+    m=0;
+  }
+}
 
 class Clock expands React.component{
 
-  
+
 
 }
 
