1c401bcfb95e7373679b1ea4ef9a6aafcbffdf68
marcelojcs <marcelojcs@outlook.com>
2019-06-27 13:34:51 -0300

    fixing mistakes

---
+++ src/notes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Notes extends React.Component {
+
+	constructor(props){
+		super(props);
+		this.state = {
+			note : note,
+			editing : false;
+		}
+	}
+
+
+}
+
+
+}
--- src/players.js
+++ src/players.js
@@ -1,67 +1,98 @@
 import React from 'react';
 var plyrs = localStorage.getItem('plyrs');
 plyrs = JSON.parse(plyrs);
-if (plyrs == null){
-  plyrs = [];
-  console.log("okay");;
-}
 class Players extends React.Component{
   constructor(props){
     super(props);
     this.state = {
+      newPlayer:{
+        name : "",
+        hp: 0,
+        maxHp: 0,
+        ac: 0,
+        dex : 0,
+        con: 0,
+        int: 0,
+        cha: 0,
+        wis : 0,
+      },
       plyrs: plyrs,
       addNew: false,
 
     }
     this.handleNewForm = this.handleNewForm.bind(this);
     this.openNewForm = this.openNewForm.bind(this);
+    this.closeNewForm = this.closeNewForm.bind(this);
+    this.rmPlyr = this.rmPlyr.bind(this);
   }
   openNewForm(){
 	this.setState({addNew : true})
 	}
+  closeNewForm(){
+	this.setState({addNew : false})
+	}
   handleNewForm(){
-    let newData ={
-		    name: this.refs.newName.value,
-        hp: this.refs.newHp.value,
-        maxHp: this.refs.newMaxHp.value,
-        ac: this.refs.newAc.value,
-        str: this.refs.newStr.value,
-        dex: this.refs.newDex.value,
-        con: this.refs.newCon.value,
-        int: this.refs.newInt.value,
-        cha: this.refs.newCha.value,
-        wis: this.refs.newWis.value,
-		};
-    this.state.plyrs.push(newData);
-    localStorage.setItem('plyrs', JSON.stringify(this.state.plyrs));
+  	let newPlyr = this.state.newPlayer;
+    this.setState({newPlayer: {
+		name: this.refs.newName.vlaue,
+        hp: this.refs.newHp.vlaue,
+        maxHp: this.refs.newMaxHp.vlaue,
+        ac: this.refs.newAc.vlaue,
+        dex: this.refs.newDex.vlaue,
+        con: this.refs.newCon.vlaue,
+        int: this.refs.newInt.vlaue,
+        cha: this.refs.newCha.vlaue,
+        wis: this.refs.newWis.vlaue,
+		}});
+    this.state.plyrs.push(this.state.newPlayer);
+    localStorage.setItem('plyrs', JSON.stringfy(this.state.plyrs));
+	this.setState({newPlayer: newPlyr});
     this.setState({ addNew: false});
  }
+  rmPlyr(index){
+  	if(window.confirm("Remove this Player?")){
+  	  let tmp = this.state.plyrs;
+		tmp.splice(index, 1);
+		this.setState({plyrs: tmp});
+		localStorage.setItem('plyrs', JSON.stringify(this.state.plyrs));
+      }
+  }
   render(){
     return(
       <div>
         <div>
+<<<<<<< HEAD
         	{(()=>{let res =[];
 
              for(let x=0; x<this.state.plyrs.length; x ++){
-               res.push(<div>
-                 <b>Name: </b> <span className="h5 border border-dark text-dark">{this.state.plyrs[x].name} </span>
-                 <b>HP: </b> <span className="h5 border border-dark text-dark">{this.state.plyrs[x].hp} </span>
-                 <b>AC: </b> <span className="h5 border border-dark text-dark">{this.state.plyrs[x].ac} </span>
-                 <b>STR: </b> <span className="h5 border border-dark text-dark">{this.state.plyrs[x].str} </span>
-                 <b>DEX: </b> <span className="h5 border border-dark text-dark">{this.state.plyrs[x].dex} </span>
-                 <b>CON: </b> <span className="h5 border border-dark text-dark">{this.state.plyrs[x].con} </span>
-                 <b>INT: </b> <span className="h5 border border-dark text-dark">{this.state.plyrs[x].int} </span>
-                 <b>CHA: </b> <span className="h5 border border-dark text-dark">{this.state.plyrs[x].cha} </span>
-                 <b>WIS: </b> <span className="h5 border border-dark text-dark">{this.state.plyrs[x].wis} </span>
-                 </div>);
+               res.push(<div className="border border-dark">
+                <div> <b>Name: </b> <span className="h5 text-dark">{this.state.plyrs[x].name} </span></div>
+                 <div><b>HP: </b> <span className="h5 text-dark">{this.state.plyrs[x].hp}</span>/<b>{this.state.plyrs[x].maxHp}  </b>|
+				 <b>AC: </b> <span className="h5 text-dark">{this.state.plyrs[x].ac} </span></div>
+                <div>
+				 <b>STR: </b> <span className="h5 text-dark">{this.state.plyrs[x].str} </span>| 
+                 <b>DEX: </b> <span className="h5 text-dark">{this.state.plyrs[x].dex} </span>|
+                 <b>CON: </b> <span className="h5 text-dark">{this.state.plyrs[x].con} </span>|
+                 <b>INT: </b> <span className="h5 text-dark">{this.state.plyrs[x].int} </span>|
+                 <b>CHA: </b> <span className="h5 text-dark">{this.state.plyrs[x].cha} </span>|
+                 <b>WIS: </b> <span className="h5 text-dark">{this.state.plyrs[x].wis} </span> </div>
+                <div className="text-right">
+                            <button className="btn h2" onClick={()=> this.rmPlyr(x)}>Remove</button>
+                </div>
+
+				 </div>);
              }
              return res;
            })()}
+=======
+        	{(()=>{return("fill me later");})()}
+>>>>>>> parent of 0869ce84... players updater
 		</div>
 		<div>
 			{(()=>{
-				if(!this.state.addNew){ return(<button className="btn btn-dark h2" onClick={this.openNewForm}>Add</button>);
+				if(!this.state.addNew){ return(<button className="btn h2" onClick={this.openNewForm}>Add New</button>);
 				}else{
+<<<<<<< HEAD
 					return(<div>
             <div><b>Name: </b> <input type="text" ref="newName" /> </div>
             <div><b>HP: </b> <input type="number" ref="newHp" /> </div>
@@ -74,10 +105,15 @@ class Players extends React.Component{
             <div><b>CHA: </b> <input type="number" ref="newCha" /> </div>
             <div><b>WIS: </b> <input type="number" ref="newWis" /> </div>
             <button className="btn btn-dark h2" onClick={this.handleNewForm}>Save</button>
+            <button className="btn h2" onClick={this.closeNewForm}>Cancel</button>
 
 
             </div>);
 
+=======
+					return(<div>Hahahahahah</div>);
+					
+>>>>>>> parent of 0869ce84... players updater
 				}
 			})()}
 		</div>
