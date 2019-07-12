af811d186bbeb3e4890dfa26d86f3f669a42de6c
marcelojcs <marcelojcs@outlook.com>
2019-07-12 00:27:12 -0300

    Frankie Test

--- src/notes.js
+++ src/notes.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+var note = localStorage.getItem('note');
 class Notes extends React.Component {
 
 	constructor(props){
 		super(props);
 		this.state = {
 			note : note,
-			editing : false;
+			editing : false,
 		}
 	}
 
