a9e0d935559f529beeac442dbed459d05acc4d5e
marcelojcs <marcelojcs@outlook.com>
2020-01-16 23:51:47 -0300

    chore(start buildinig overlay): begun to build overlay  Element on React

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 import Clock from  './clock';
 import DiceRoller from './diceRoller';
 import Players from './players';
+import Overlay from './overlay';
 import './App.css';
 
 class App extends React.Component{
  render(){ return (
  	   <div>
 
-  	     <Clock />
-  		<DiceRoller />
-      <Players />
+  	     	<Clock />
+  			<DiceRoller />
+      		<Players />
 
 	    </div>
 	  );
